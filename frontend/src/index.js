7353019b50e37d01cc4c07b3323587983283adad
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-11-29 10:44:34 +0400

    Add index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,1 @@
+import React from 'react';\nimport { createRoot } from 'react-dom/client';\nimport App from './App';\n// TODO: wrap in AuthProvider if created\ncreateRoot(document.getElementById('root')).render(<App />);\n
